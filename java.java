ebbc4f1b829a60caae1f300dc10e8219dc721b83
“cpdrenato” <cpdrenato@gmail.com>
2018-04-05 14:34:58 -0300

    grid teste

---
+++ java.java
@@ -0,0 +1,34 @@
+
+public class OlaMundo {
+  public static void main(String[] args){
+    System.out.println("Ola mundo");
+  }
+                          
+}
+
+
+
+public class OlaMundo {
+  public static void main(String[] args){
+    System.out.println("Ola mundo");
+    System.out.println("\n*************************");
+	System.out.println("Sejam bem-vindos ao Java!");
+	System.out.println("*************************\n");
+  }
+                          
+}
+
+
+public class OlaMundo {
+  public static void main(String[] args){
+    System.out.println("Ola mundo");
+    System.out.println("\n*************************");
+	System.out.println("Sejam bem-vindos ao Java!");
+	System.out.println("*************************\n");
+    System.out.printf("%s\n%s\n", "Bem-vindo ", " a UFABC ");
+    System.out.printf("%s\n%s%d\n%s%f\n", "Nome: Leidy", "Idade: ", 29,
+"Altura: ", 1.59);
+    System.out.printf("Nome: %s\nIdade: %d\nAltura: %3.2f\n", "Maria", 19, 1.69);
+  }
+                          
+}
